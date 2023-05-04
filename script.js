e57c0d55583efd82545d27b14ea9a922ad31afa1
busyc <11322534+busyc@users.noreply.github.com>
2023-05-05 00:29:16 +0200

    Add 3s countdown

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var clickCount = 0;
 
 document.getElementById('generate-btn').addEventListener('click', function() {
-    var randomNumber = Math.floor(Math.random() * 100) + 1;
-    document.getElementById('random-number').innerHTML = randomNumber;
+    var button = document.getElementById('generate-btn');
+    button.classList.add('loading');
 
-    clickCount++;
-    document.getElementById('click-counter').innerHTML = 'Button clicks: ' + clickCount;
+    setTimeout(function() {
+        var randomNumber = Math.floor(Math.random() * 100) + 1;
+        document.getElementById('random-number').innerHTML = randomNumber;
+        button.classList.remove('loading');
+
+        clickCount++;
+        document.getElementById('click-counter').innerHTML = 'Button clicks: ' + clickCount;
+    }, 3000);
 });
