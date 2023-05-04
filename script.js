4cc12ddcdff86862a953a4d314b3a30a7848835e
busyc <11322534+busyc@users.noreply.github.com>
2023-05-05 00:15:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+var clickCount = 0;
+
 document.getElementById('generate-btn').addEventListener('click', function() {
     var randomNumber = Math.floor(Math.random() * 100) + 1;
     document.getElementById('random-number').innerHTML = randomNumber;
+
+    clickCount++;
+    document.getElementById('click-counter').innerHTML = 'Button clicks: ' + clickCount;
 });
