63a31f491b685a09a746be3567d9ead1155499c3
busyc <11322534+busyc@users.noreply.github.com>
2023-05-07 12:01:41 +0200

    Updated probabilities

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
+/* 
 function rollSlot(slot) {
   const min = 0;
   const max = 9;
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   slot.textContent = randomNumber;
+}
+*/
+
+function rollSlot(slot) {
+  const min = 0;
+  const max = 9;
+
+  // Define the custom probability distribution
+  const probabilities = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1];
+  probabilities[7] = 0.4; // Set a higher probability for the number 7
+
+  // Generate a random number based on the custom probability distribution
+  const randomNumber = getRandomNumberWithProbability(min, max, probabilities);
+
+  slot.classList.add('rolling');
+  setTimeout(() => {
+    slot.textContent = randomNumber;
+    slot.classList.remove('rolling');
+  }, 1000);
+}
+
+function getRandomNumberWithProbability(min, max, probabilities) {
+  const random = Math.random();
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += probabilities[i];
+    if (random <= sum) {
+      return i;
+    }
+  }
+
+  return max; // Fallback in case the random number is not covered by the probabilities
 }
 
 function checkWinning(slots) {
